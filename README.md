27ecf28a15a8cf1915aadb9715ac0dbb57ec2753
DanBaDo <daniel.banobre.dopico@protonmail.com>
2021-11-08 16:30:24 +0000

    First try

--- README.md
+++ README.md
@@ -1,51 +1,14 @@
-# Ejercicios propuestos para el 03/11/2021
-## Preparaós para sufrir un poquito ;) 
+# Arrays
 
-### ¿Cómo comenzar el proyecto?
+## Ejercicio 1
+No hay mucho que comentar
 
-1. Crea un nuevo WorkSpace en GITPOD
-2. Haz un fork del proyecto.
-3. Clona el fork en tu GITPOD `git clone <url>`
-4. Comienza a trabajar :) 
+## Ejercicio 2
+Tampoco hay mucho que comentar... pero tengo que revisarlos en el futuro si me lo puedo permitir.
 
-### ¿Cómo ejecutar el proyecto?
+## Ejercicio 3
+Ya que usamos un objeto para proporcionar los puntos, parece que tiene sentido usar un objeto para proporcionar la distancia.
 
-1. Situate en el directorio del proyecto.
-2. Ejecuta `node index.js`
-3. Verás los resultados por terminal.
-
-### Instrucciones 
-
-- Código documentado
-- Aplicando programación funcional (Separado en funciones)
-- La declaracion de funciones y variables en INGLES
-- Aplicando escritura camel case.
-- Elaborar un archivo README explicando lo hecho y incluyendo las instrucciones para ejecutar el proyecto.
-
-### Ejercicios
-
-**PISTA: Antes de usar los arrays deberás limpiarlo**
-
-1. Crea una funcion que encuentre el valor 4 en el array exampleArray 
- - Debes crear un algoritmo usando programación funcional 
- - Debes crear otro algoritmo usando programación imperativa (filter, split, reduce o cualquierda de ellas que manipulen arrays)
-2. Crea una funcion que limpie el array shit usando programación declarativa (split, join o cualquiera de ellas). La función solo debe dejar los strings como resultado.
-3. Crea una función que calcule la distancia entre dos puntos usando el objeto que yo te proporcionono como Point, debes usar programación imperativa para este ejericio. Tu funcion debe recibir dos parametros, punto a y punto b y calcular la distancia.
-4. Crear una función que compare los valores de exampleArray2 y exampleArray1 pero que se fije en el tipo
-
-### ¿Cómo entregar el proyecto?
-
-1. Finaliza todos los ejercicios propuestos y asegurate de que están comentados :) 
-2. Escribeme por Slack @Manu y enviame el Link de tu repo para poder comprobar los ejercicios. -> Dijo en generador de dominios
-3. Espera mi respuesta o tus amigos ISSUES por GITHUB
-
-### Contribuciones
-
-Si ves algun fallo en estas instrucciones recuerda que soy **HUMANO**
-Puedes hacer un PULL-REQUEST y estaré encantado de aceptartelo siempre y cuando contenga información relevante. 
-
-### Ultimas cosas
-
-Recuerda que soy un maniático del orden y la jerarquía, asegurate que tus funciones están optimizadas, comentadas y tabuladas correctamente, si no tendrás que corregirlo.
-
-Divierte escribiendo código en JS a mi salud!!
+## Ejercicio 4
+Procuro clasificar todos los resultados en base a los criterios de similitud que se me ocurrieron.
+Se puede mejorar añadiendo algunas listas para acceder a los elementos de las mismas categorías, por ejemplo.
--- index.js
+++ index.js
@@ -69,16 +69,112 @@ console.log(`Ejercicio 1 declarativo: ${findInArrayDeclarative(exampleArray,4)}`
  */
 
 /**
- * 
+ * Retorna un array con los string encontrados en el array proporcionado
  * @param {*} array 
- * @returns 
+ * @returns {String} array
  */
 function getStrings(array) {
-    return array.filter(
-        item => typeof(item) === "string"
-    )
+    if ( Array.isArray(array) ) {
+        return array.filter(
+            item => typeof(item) === "string"
+        )
+    } else {
+        console.error('You must to provide a array.')
+        return false
+    }
 }
 /* Tests */
 console.log('Ejercicio 2:', getStrings(shit))
 
+/**
+ * Ejercicio 3
+ */
 
+/**
+ * Distance between tow point in x and y axis.
+ * @param {number} x 
+ * @param {number} y 
+ */
+function Distance(x,y) {
+    this.x = x;
+    this.y = y;
+}
+
+/**
+ * Get two points and return x and y distance.
+ * @param {Point} p1 
+ * @param {Point} p2 
+ * @returns {Distance||null} Return 
+ */
+function getDistance(p1, p2) {
+    return p1 instanceof Point && p2 instanceof Point ? new Distance(Math.abs(p1.x - p2.x), Math.abs(p1.y - p2.y)) : new Error('Arguments must be Point instances')
+}
+
+// Tests
+const p1 = new Point(1,2)
+const p2 = new Point(3,1)
+console.log('Ejerecicio 3:',getDistance(p1, p2))
+//console.log(getDistance(p1, 55))
+
+/**
+ * Ejercicio 4
+ */
+
+/**
+ * Distance between tow point in x and y axis.
+ * @static {object} resultCodes nemonic-numeric mapping result codes 
+ * @param {Result.resultCodes.*} code Comparation result: >=0 equal <0 different
+ * @param {number} path Path to element relative to root array
+ */
+class Result {
+    static resultCodes = {
+        reference: 0,
+        sameContents: 1,
+        noArray: -1,
+        differentLength: -2,
+        differentContents: -3
+    }
+    constructor (code, path) {
+        this.code = code;
+        this.path = path;
+    }
+}
+
+/**
+ * Compares two arrays and outputs a numeric result code.
+ * @param {Array} array First array for comparison
+ * @param {Array} referenceArray Second array for comparison
+ * @returns {Result.resultCodes} 
+ */
+function compareArrays(array, referenceArray) {
+    if ( array.length !== referenceArray.length ) return Result.resultCodes.differentLength;
+    if ( array === referenceArray ) return Result.resultCodes.reference;
+    return array.every( (item, idx) => item === referenceArray[idx] ) ? Result.resultCodes.sameContents : Result.resultCodes.differentContents;
+}
+
+/**
+ * 
+ * @param {*} referenceArray 
+ * @param {*} arrayForSearchOn 
+ * @param {*} results 
+ * @param {*} path 
+ * @returns 
+ */
+function findEqualArrays(referenceArray, arrayForSearchOn, results = [], path = []) {
+    if ( !Array.isArray(referenceArray) ) return new Error('Reference array... must be an array');
+    if ( !Array.isArray(arrayForSearchOn) ) results.push(new Result(Result.resultCodes.noArray, path))
+    else {
+        results.push(new Result(compareArrays(referenceArray, arrayForSearchOn),path));
+        arrayForSearchOn.forEach( (item, idx) => findEqualArrays(referenceArray, item, results, [...path, idx]) ) // Implicit returns kills instance
+    }
+    return results;
+}
+
+// Tests
+const exampleArray3 = [
+    [10, 4, 100, 35, 31, 23, 443, 221, 342, 10, 12, 42],
+    exampleArray,
+    [10, 4, "100", 35, "31", "23", 443, "221", "342", 10, 12, 42],
+]
+console.log("Ejercicio 4:",JSON.stringify(findEqualArrays(exampleArray, exampleArray3)))
+console.log("Ejercicio 4:",JSON.stringify(findEqualArrays(exampleArray, exampleArray2)))
